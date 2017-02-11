a959102ff144cb3fba80d6066120ec2ee16f4a6c
Jonas Stumph Stevnsvig <jonas.stumph@stevnsvig.com>
2017-02-11 11:10:50 +0100

    fixed erroneous status message blocking async function status message

--- js/lights.js
+++ js/lights.js
@@ -1,21 +1,19 @@
 var lightsApp = angular.module('lightsApp', ['ui.bootstrap','angular-loading-bar']);
-
 lightsApp.controller('DeviceListCtrl', function ($scope, $http) {
  $scope.loadData = function(){
-   //console.log("Load Called");
+   console.log("Load Called @"+ (new Date()));
    $scope.status = "Loading data";
    $http.get('tdaction.php?action=listDevices').success(function(data) {
      $scope.devices = data;
      $scope.status = "";
+     console.log("data fetched @"+ (new Date()))
    });
  };
 
  $scope.tdaction = function(id,action) {
    $scope.status = "Turning "+action+" Reloading Data" ;
-   //console.log(id + " "+ action);
    $http.get("tdaction.php?id="+id+"&action="+action);
    $scope.loadData();
-   $scope.status ="";
  };
  
  $scope.allOff = function(){
