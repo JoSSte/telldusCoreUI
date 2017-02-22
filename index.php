14f25cd150068c5438921a191875cdfec71d5761
Jonas Stumph Stevnsvig <jonas@stevnsvig.com>
2017-02-22 10:19:25 +0100

    sanitized inputs & formatting

--- index.php
+++ index.php
@@ -1,70 +1,70 @@
 <!doctype html>
 <html>
-<head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.5.9/angular.min.js"></script>
-    <!-- Latest compiled and minified CSS -->
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
-    <link rel="stylesheet" href="style/loading-bar.min.css">
-    <link rel="stylesheet" href="style/lights.css">
-    <script src="js/ui-bootstrap-tpls-2.5.0.min.js"></script>
-    <script src="js/loading-bar.min.js"></script>
-    <script src="js/lights.js"></script>
-    <title>lights</title>
-</head>
-<body>
-<nav class="navbar navbar-inverse navbar-fixed-top">
-      <div class="container">
-        <div class="navbar-header">
-          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
-            <span class="sr-only">Toggle navigation</span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-          </button>
-          <a class="navbar-brand" href="">Lights</a>
-        </div>
-        <div id="navbar" class="collapse navbar-collapse">
-          <ul class="nav navbar-nav">
-            <li class="active"><a href="#">Control</a></li>
-            <!-- <li><a href="#about">About</a></li>
-            <li><a href="#contact">Contact</a></li> -->
-          </ul>
-        </div><!--/.nav-collapse -->
-      </div>
-    </nav>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.5.9/angular.min.js"></script>
+        <!-- Latest compiled and minified CSS -->
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+        <link rel="stylesheet" href="style/loading-bar.min.css">
+        <link rel="stylesheet" href="style/lights.css">
+        <script src="js/ui-bootstrap-tpls-2.5.0.min.js"></script>
+        <script src="js/loading-bar.min.js"></script>
+        <script src="js/lights.js"></script>
+        <title>lights</title>
+    </head>
+    <body>
+        <nav class="navbar navbar-inverse navbar-fixed-top">
+            <div class="container">
+                <div class="navbar-header">
+                    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+                        <span class="sr-only">Toggle navigation</span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </button>
+                    <a class="navbar-brand" href="">Lights</a>
+                </div>
+                <div id="navbar" class="collapse navbar-collapse">
+                    <ul class="nav navbar-nav">
+                        <li class="active"><a href="#">Control</a></li>
+                        <!-- <li><a href="#about">About</a></li>
+                        <li><a href="#contact">Contact</a></li> -->
+                    </ul>
+                </div><!--/.nav-collapse -->
+            </div>
+        </nav>
 
-    <div class="container">
-        <div class="starter-template">
-            <div ng-app = "lightsApp" ng-controller="DeviceListCtrl" data-ng-init="loadData()">
+        <div class="container">
+            <div class="starter-template">
+                <div ng-app = "lightsApp" ng-controller="DeviceListCtrl" data-ng-init="loadData()">
 
-              <table ng-table="devicesTable" class="table table-striped">
-                <thead>
-                  <tr>
-                    <th class="ng-hide">ID</th>
-                    <th>Name</th>
-                    <th>Last CMD</th>
-                    <th colspan="2" class="center">Commands</th>
-                  </tr>
-                </thead>
-                <tbody>
-                  <tr ng-repeat="x in devices">
-                    <td class="ng-hide">{{x.id}}</td>
-                    <td>{{x.name}}</td>
-                    <td>{{x.lastcommand}}</td>
-                    <td><button class="btn btn-success" ng-click="tdaction(x.id,'on')">ON</button></td>
-                    <td><button class="btn btn-danger" ng-click="tdaction(x.id,'off')">OFF</button></td>
-                  </tr>
-               </tbody>
-             </table>
-           <!-- todo: turn off all button -->
-        <button type="button" class="btn btn-primary btn-danger" ng-click="allOff()">All OFF</button>
-        <br>
-        {{status}}
+                    <table ng-table="devicesTable" class="table table-striped">
+                        <thead>
+                            <tr>
+                                <th class="ng-hide">ID</th>
+                                <th>Name</th>
+                                <th>Last CMD</th>
+                                <th colspan="2" class="center">Commands</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr ng-repeat="x in devices">
+                                <td class="ng-hide">{{x.id}}</td>
+                                <td>{{x.name}}</td>
+                                <td>{{x.lastcommand}}</td>
+                                <td><button class="btn btn-success" ng-click="tdaction(x.id,'on')">ON</button></td>
+                                <td><button class="btn btn-danger" ng-click="tdaction(x.id,'off')">OFF</button></td>
+                            </tr>
+                        </tbody>
+                    </table>
+                    <!-- todo: turn off all button -->
+                    <button type="button" class="btn btn-primary btn-danger" ng-click="allOff()">All OFF</button>
+                    <br>
+                    {{status}}
+                </div>
+            </div>
         </div>
-      </div>
-    </div>
-</body>
+    </body>
 </html>
--- tdaction.php
+++ tdaction.php
@@ -1,63 +1,59 @@
 <?php
-//TODO: sanitize inputs
+
 //TODO: return response codes
 //TODO: allowmultiple ids/device groups
-
 // https://lassesunix.wordpress.com/2013/12/28/decoding-nexa-remotes-with-tellstick-duo/
-$action = $_GET["action"];
-$id = $_GET["id"];
+$action = filter_input(INPUT_GET, "action", FILTER_SANITIZE_STRING, FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+$id = filter_input(INPUT_GET, "id", FILTER_SANITIZE_NUMBER_INT);
 
-switch ($action){
-  case "on":
-  case "off":
-    $cmd = "tdtool --$action $id";
-    exec($cmd);
-    error_log($cmd." executed");
-    break;
-  case "listDevices":
-    header("Content-Type: application/json");
-    //return JSON
-    echo json_encode(getAllDevices());
-    break;
-  case "listSensors":
-    header("Content-Type: application/json");
-    //return JSON
-    echo json_encode(getAllSensors());
-    break;
-  case "allOff":
-    $alldevs = getAllDevices();
-    foreach($alldevs as $dev){
-      exec("tdtool --off ".$dev["id"]);
+switch ($action) {
+    case "on":
+    case "off":
+        $cmd = "tdtool --$action $id";
+        exec($cmd);
+        error_log($cmd . " executed");
+        break;
+    case "listDevices":
+        header("Content-Type: application/json");
+        //return JSON
+        echo json_encode(getAllDevices());
+        break;
+    case "listSensors":
+        header("Content-Type: application/json");
+        //return JSON
+        echo json_encode(getAllSensors());
+        break;
+    case "allOff":
+        $alldevs = getAllDevices();
+        foreach ($alldevs as $dev) {
+            exec("tdtool --off " . $dev["id"]);
+        }
+        break;
+}
+
+function getAllDevices() {
+    //TODO: investigate multi threaded activity here: http://stackoverflow.com/questions/70855/how-can-one-use-multi-threading-in-php-applications
+    //get devices from tdtool
+    $devs = shell_exec("tdtool --list-devices");
+
+    //turn raw result into array
+    $devicelist = array();
+    preg_match_all('/type=(?<type>\w+)\tid=(?<id>\d+)\tname=(?<name>[a-zA-ZæøåÆØÅ\-]+)\tlastsentcommand=(?<lastcommand>\w+)/', $devs, $matches);
+    for ($x = 0; $x < count($matches[0]); $x++) {
+        $devicelist[] = array("id" => $matches["id"][$x], "name" => $matches["name"][$x], "lastcommand" => $matches["lastcommand"][$x]);
     }
-    break;
-
+    return $devicelist;
 }
 
+function getAllSensors() {
+    //get devices from tdtool
+    $sens = shell_exec("tdtool --list-sensors");
 
-function getAllDevices(){
-  //TODO: investigate multi threaded activity here: http://stackoverflow.com/questions/70855/how-can-one-use-multi-threading-in-php-applications
-  //get devices from tdtool
-  $devs = shell_exec("tdtool --list-devices");
-
-  //turn raw result into array
-  $devicelist = array();
-  preg_match_all('/type=(?<type>\w+)\tid=(?<id>\d+)\tname=(?<name>[a-zA-ZæøåÆØÅ\-]+)\tlastsentcommand=(?<lastcommand>\w+)/', $devs, $matches);
-  for ($x = 0 ; $x < count($matches[0]);$x++){
-    $devicelist[] = array("id"=> $matches["id"][$x],"name"=>$matches["name"][$x],"lastcommand"=>$matches["lastcommand"][$x]);
-  }
-  return $devicelist;
+    //turn raw result into array
+    $sensorlist = array();
+    preg_match_all('/type=(?<type>\w+)\tprotocol=(?<protocol>\w+)\tmodel=(?<model>\w+)\tid=(?<id>\d+)\ttemperature=(?<temperature>[\d\.]+)(|\thumidity=(?<humidity>[\d\.]+))\ttime=(?<time>[\d-\s:]+)\tage=(?<age>\d+)/', $sens, $matches);
+    for ($x = 0; $x < count($matches[0]); $x++) {
+        $sensorlist[] = array("type" => $matches["type"][$x], "protocol" => $matches["protocol"][$x], "id" => $matches["id"][$x], "temperature" => $matches["temperature"][$x], "humidity" => $matches["humidity"][$x], "time" => $matches["time"][$x], "age" => $matches["age"][$x]);
+    }
+    return $sensorlist;
 }
-
-function getAllSensors(){
-  //get devices from tdtool
-  $sens = shell_exec("tdtool --list-sensors");
-
-  //turn raw result into array
-  $sensorlist = array();
-  preg_match_all('/type=(?<type>\w+)\tprotocol=(?<protocol>\w+)\tmodel=(?<model>\w+)\tid=(?<id>\d+)\ttemperature=(?<temperature>[\d\.]+)(|\thumidity=(?<humidity>[\d\.]+))\ttime=(?<time>[\d-\s:]+)\tage=(?<age>\d+)/', $sens, $matches);
-  for ($x = 0 ; $x < count($matches[0]);$x++){
-    $sensorlist[] = array("type"=> $matches["type"][$x],"protocol"=>$matches["protocol"][$x],"id"=>$matches["id"][$x],"temperature"=>$matches["temperature"][$x],"humidity"=>$matches["humidity"][$x],"time"=>$matches["time"][$x],"age"=>$matches["age"][$x]);
-  }
-  return $sensorlist;
-}
-
