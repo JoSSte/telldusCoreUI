e66f8a3582b0ee0b4a78616b51fb772948c78840
Jonas Stumph Stevnsvig <jonas@stevnsvig.com>
2022-11-26 21:19:38 +0100

    added usage comment

--- test.py
+++ test.py
@@ -2,6 +2,8 @@ import time
 import random
 import callbackService
 
+# remember to set DEBUG=True in .env
+
 for x in range(10):
   callbackService.sensor_event( '', '', '999', 'temperature', random.randint(-2900, -190)/10, int(time.time()), '-666')
   time.sleep(1)
