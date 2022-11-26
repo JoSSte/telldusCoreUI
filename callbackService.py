1b4e7371bc29400a2fac4c551553bfb80876d6aa
Jonas Stumph Stevnsvig <jonas@stevnsvig.com>
2022-11-26 21:13:30 +0100

    implementing  cache functions

--- callbackService.py
+++ callbackService.py
@@ -2,12 +2,15 @@
 # -*- coding: utf-8 -*-
 
 import mysql.connector
+import sqlite3
 import tellcore.telldus as td
 import tellcore.constants as const
 import os
 from datetime import date, datetime, timedelta
 from mysql.connector import errorcode
 from dotenv import load_dotenv
+
+from sqlite3 import Error
 
 load_dotenv()
 
@@ -16,6 +19,7 @@ MYSQL_PASSWORD = os.getenv('MYSQL_PASSWORD')
 MYSQL_HOST = os.getenv('MYSQL_HOST')
 MYSQL_DB = os.getenv('MYSQL_DB')
 SQLITE_DB = 'localcache.db'
+DEBUG=os.getenv('DEBUG')
 
 
 # initialize SQlite database for local cache
@@ -32,19 +36,6 @@ CREATE TABLE IF NOT EXISTS sensorData (
 """)
 conn.close()
 
-def cache_data(protocol, model, id_, dataType, value, timestamp, cid):
-  conn = sqlite3.connect(SQLITE_DB)
-  c = conn.cursor()
-  print('caching. . . ', end='')
-  try:
-    c.execute("INSERT INTO sensorData (sensorid, timestamp, value, valuetype) values (?,?,?,?)", (id_, timestamp, value, dataType))
-    conn.commit()
-    print('cached. . .  ', end='')
-  except Error as e:
-    print("Error caching " + e)
-  else:
-    conn.close()
-
 METHODS = {const.TELLSTICK_TURNON: 'turn on',
            const.TELLSTICK_TURNOFF: 'turn off',
            const.TELLSTICK_BELL: 'bell',
@@ -56,9 +47,57 @@ METHODS = {const.TELLSTICK_TURNON: 'turn on',
            const.TELLSTICK_DOWN: 'down',
            const.TELLSTICK_STOP: 'stop'}
 
-#Callback handler Sensor Event
+def cache_data(protocol, model, id_, dataType, value, timestamp, cid):
+  conn1 = sqlite3.connect(SQLITE_DB)
+  cur1 = conn1.cursor()
+  print('caching. . . ', end='')
+  try:
+    cur1.execute("INSERT INTO sensorData (sensorid, timestamp, value, valuetype) values (?,?,?,?)", (id_, timestamp, value, dataType))
+    conn1.commit()
+    print('cached. . .  ', end='')
+  except Error as e:
+    print("Error caching %s" % (' '.join(e.args)))
+  else:
+    conn1.close()
+  
+# called only if database is there
+def check_cache():
+  print("checking if there are cached entries")
+  conn2 = sqlite3.connect(SQLITE_DB)
+  cur2 = conn2.cursor()
+  try:
+    cur2.execute("SELECT sensorid, valuetype, value, timestamp FROM sensorData")
+    rows = cur2.fetchall()
+    conn2.close()
+    print("Found %d cached entries" % (len(rows)))
+    for row in rows:
+      ret = sensor_event('', '', row[0], row[1], row[2], row[3], '', False)
+      # abort if database goes away
+      if ret <0:
+        break
+      else:
+        print("deleting entry %s %s %s %s " % (row[0], row[1], row[2], row[3]))
+        delete_cache_entry('', '', row[0], row[1], row[2], row[3], '')
+  except Error as e:
+    print("Error handling cache %s" % (' '.join(e.args)))
+  else:
+    conn2.close()
 
-def sensor_event(protocol, model, id_, dataType, value, timestamp, cid):
+def delete_cache_entry(protocol, model, id_, dataType, value, timestamp, cid):
+  conn3 = sqlite3.connect(SQLITE_DB)
+  cur3 = conn3.cursor()
+  try:
+    cur3.execute("DELETE FROM sensorData WHERE sensorid =? AND valuetype =? AND value =? AND timestamp =?", (id_, timestamp, value, dataType))
+    conn3.commit()
+  except Error as e:
+    print("Error Deleting cache entry %s" % (' '.join(e.args)))
+  else:
+    conn3.close()
+    print("deleted")
+
+# Callback handler Sensor Event
+def sensor_event(protocol, model, id_, dataType, value, timestamp, cid, cache_enable=True):
+  #print("Sensorevent (protocol: %s, model: %s, id_: %s, dataType: %s, value: %s, timestamp: %s, cid: %s)"%(protocol, model, id_, dataType, value, timestamp, cid))
   try:
     # output of '..' means it went well
     print('.', end='')
@@ -66,27 +105,35 @@ def sensor_event(protocol, model, id_, dataType, value, timestamp, cid):
     cursor = cnx.cursor()
     add_temperature = ("INSERT INTO sensorData (sensorid, timestamp, value, valuetype) values (%s,from_unixtime(%s),%s,%s)")
     data_temperature = (id_, timestamp, value, dataType)
-
     #Insert temperature data
     cursor.execute(add_temperature, data_temperature)
     cnx.commit()
+    if cache_enable:
+        check_cache()
   except mysql.connector.Error as err:
     if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
       print("Something is wrong with your user name (%s) or password. Caching data for later. . . " % (MYSQL_USER), end='')
-      cache_data(protocol, model, id_, dataType, value, timestamp, cid)
+      if cache_enable:
+        cache_data(protocol, model, id_, dataType, value, timestamp, cid)
     elif err.errno == errorcode.ER_BAD_DB_ERROR:
       print("Database %s does not exist on host %s. Caching data for later. . . " % (MYSQL_DB, MYSQL_HOST), end='')
-      cache_data(protocol, model, id_, dataType, value, timestamp, cid)
+      if cache_enable:
+        cache_data(protocol, model, id_, dataType, value, timestamp, cid)
     elif err.errno == errorcode.CR_CONN_HOST_ERROR:
       print("Connection Issue: %s - potential network error. Caching data for later. . . " % (err.msg), end='')
-      cache_data(protocol, model, id_, dataType, value, timestamp, cid)
+      if cache_enable:
+        cache_data(protocol, model, id_, dataType, value, timestamp, cid)
+    else:
+      print(err)
+    return -1
   else:
     cursor.close()
     cnx.close()
   # output of '..' means it went well
   print('.')
+  return 0
 
-#Callback handler Button Event
+# Callback handler Button Event
 def device_event(id_, method, data, cid):
     method_string = METHODS.get(method, "UNKNOWN METHOD {0}".format(method))
     string = "[DEVICE] {0} -> {1}".format(id_, method_string)
@@ -94,31 +141,32 @@ def device_event(id_, method, data, cid):
         string += " [{0}]".format(data)
     print(string)
 
-#Callback handling
-try:
-    import asyncio
-    loop = asyncio.get_event_loop()
-    dispatcher = td.AsyncioCallbackDispatcher(loop)
-except ImportError:
-    loop = None
-    dispatcher = td.QueuedCallbackDispatcher()
-
-core = td.TelldusCore(callback_dispatcher=dispatcher)
-callbacks = []
-
-#register callback for sensor event
-callbacks.append(core.register_sensor_event(sensor_event))
-callbacks.append(core.register_device_event(device_event))
-
-#Handle events
-try:
-    if loop:
-        loop.run_forever()
-    else:
-        import time
-        while True:
-            core.callback_dispatcher.process_pending_callbacks()
-            time.sleep(0.5)
-            print('-')
-except KeyboardInterrupt:
-    pass
+if DEBUG == False:
+  # Callback handling
+  try:
+      import asyncio
+      loop = asyncio.get_event_loop()
+      dispatcher = td.AsyncioCallbackDispatcher(loop)
+  except ImportError:
+      loop = None
+      dispatcher = td.QueuedCallbackDispatcher()
+  
+  core = td.TelldusCore(callback_dispatcher=dispatcher)
+  callbacks = []
+  
+  #register callback for sensor event
+  callbacks.append(core.register_sensor_event(sensor_event))
+  callbacks.append(core.register_device_event(device_event))
+  
+  #Handle events
+  try:
+      if loop:
+          loop.run_forever()
+      else:
+          import time
+          while True:
+              core.callback_dispatcher.process_pending_callbacks()
+              time.sleep(0.5)
+              print('-')
+  except KeyboardInterrupt:
+      pass
